b7aa230dc330e29c47c9f09db9defdea0822e325
Soporte Técnico de Educación a Distancia <103531425+soporteead@users.noreply.github.com>
2025-07-03 08:40:40 -0600

    Cambios de Prueba

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,17 @@
 <template>
 	<h1>This is the login page</h1>
+	<div class="flex flex-col items-center p-7 rounded-2xl">
+  <div>
+    <img class="size-48 shadow-xl" alt="" src="/img/cover.png" />
+  </div>
+  <div class="flex">
+    <span>Class Warfare</span>
+    <span>The Anti-Patterns</span>
+    <span class="flex">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+  </div>
+</div>
 </template>
