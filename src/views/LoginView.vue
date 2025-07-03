752fe62e38c6731721d6e9c4d5d43822613b6f78
Soporte Técnico de Educación a Distancia <103531425+soporteead@users.noreply.github.com>
2025-07-03 08:47:11 -0600

    Prueba2

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,7 +2,7 @@
 	<h1>This is the login page</h1>
 	<div class="flex flex-col items-center p-7 rounded-2xl">
   <div class="flex">
-    <span>Class Warfare</span>
+<span>Class Warfare</span>
     <span>The Anti-Patterns</span>
     <span class="flex">
       <span>No. 4</span>
