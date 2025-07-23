48d8b5e1fc0b831216a0e5e53099f3db57c49ea3
ezio19r <ric-reyes@outlook.com>
2025-07-23 12:00:49 -0600

    feat :sparkles: (states.js)Added: ShowState, RemoveState, CreateState, and UpdateState services

--- src/services/location/states.js
+++ src/services/location/states.js
@@ -12,3 +12,55 @@ export function getStates() {
 			});
 	});
 }
+
+export function showState(state_id) {
+	return new Promise((resolve, reject) => {
+		axios
+			.get(`states/${state_id}`)
+			.then((response) => {
+				resolve(response);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+}
+
+export function updateState(state_id) {
+	return new Promise((resolve, reject) => {
+		axios
+			.put(`states/${state_id}`)
+			.then((response) => {
+				resolve(response);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+}
+
+export function removeState(state_id) {
+	return new Promise((resolve, reject) => {
+		axios
+			.delete(`states/${state_id}`)
+			.then((response) => {
+				resolve(response);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+}
+
+export function createState(params) {
+	return new Promise((resolve, reject) => {
+		axios
+			.post('states', params)
+			.then((response) => {
+				resolve(response);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+}
