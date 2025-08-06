f0e85886472ef848a1c0de20cb0237bc3c601804
ezio19r <ric-reyes@outlook.com>
2025-08-06 11:16:58 -0600

    feat :sparkles: Backend Route

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      vue(),
-  ],
-})
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://edutech.aguascalientes.tecnm:50000',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			},
+		},
+	},
+	plugins: [vue()],
+});
